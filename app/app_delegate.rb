84ee1f02868d43396e9b8ab39aad1b42be01cd79
toshiwo <toshiwo@toshiwo.com>
2014-02-16 23:50:14 +0900

    implementation KeyEventManager and TinyWindow

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,3 +1,7 @@
+class TinyWindow < NSWindow
+  def canBecomeKeyWindow; true; end
+end
+
 class AppDelegate
   def applicationDidFinishLaunching(notification)
     buildMenu
@@ -5,11 +9,21 @@ class AppDelegate
   end
 
   def buildWindow
-    @mainWindow = NSWindow.alloc.initWithContentRect([[240, 180], [480, 360]],
+    @mainWindow = TinyWindow.alloc.initWithContentRect([[240, 180], [480, 360]],
       styleMask: NSTitledWindowMask|NSClosableWindowMask|NSMiniaturizableWindowMask|NSResizableWindowMask,
       backing: NSBackingStoreBuffered,
       defer: false)
     @mainWindow.title = NSBundle.mainBundle.infoDictionary['CFBundleName']
+
+    initialize_firest_responder
+
     @mainWindow.orderFrontRegardless
   end
+
+  def initialize_firest_responder
+    @key_event_manager = KeyEventManager.new
+    @mainWindow.makeFirstResponder @key_event_manager
+  end
+  private :initialize_firest_responder
+
 end
---
+++ app/models/key_event_manager.rb
@@ -0,0 +1,14 @@
+class KeyEventManager < NSResponder
+
+  private
+
+  def keyUp event
+  end
+
+  def keyDown event
+  end
+
+  def flagsChanged event
+  end
+
+end
